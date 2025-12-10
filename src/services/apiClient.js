672b075d985e35919a581173657179d4a7e15b8b
ansh-2609 <anshku037@gmail.com>
2025-12-10 13:56:39 +0530

    fixes

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,10 @@
 export const apiFetch = (url, options = {}) => {
   return fetch(url, {
     credentials: "include",
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers
+    },
     ...options
   });
 };
